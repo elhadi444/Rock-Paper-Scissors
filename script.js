fdf3771d1032db3fb8a9b640f20fb6f7aab1d8f6
elhadi <elhadiezzerougezzraimi@gmail.com>
2023-02-13 15:09:58 +0100

    rps + ui

--- script.js
+++ script.js
@@ -1,74 +1,96 @@
+let score = [0,0]
+
+const btnR = document.getElementById("r")
+const btnP = document.getElementById("p")
+const btnS = document.getElementById("s")
+const scoreDiv = document.getElementById("score")
+const gameCommentDiv = document.getElementById("game-comment")
+const retryDiv = document.getElementById("retry")
+
+function newGame(){
+    btnR.disabled = false
+    btnP.disabled = false  
+    btnS.disabled = false
+    scoreDiv.innerHTML = ''
+    gameCommentDiv.innerHTML = ''
+    retryDiv.innerHTML = ''
+    score = [0,0]
+}
+
 function getComputerChoice(){
     let n = parseInt(Math.random() * 10)
     if(n < 3)
-        return "rock"
+        return "r"
     else if(n >= 6)
-        return "paper"
+        return "p"
     else 
-        return "scissors"
+        return "s"
 }
 
-function getPlayerChoice(){
-    let PlayerChoice = prompt("Write 'Rock' 'Paper' or 'Scissors'")
-    while(PlayerChoice.toLowerCase() != "rock" && PlayerChoice.toLowerCase() != "paper" && 
-            PlayerChoice.toLowerCase() != "scissors"){
-        PlayerChoice = prompt("You can only write 'Rock' 'Paper' or 'Scissors'")
-    }
-    return PlayerChoice.toLowerCase()
-}
-
-function playRound(playerSelection, computerSelection, score) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) 
         return "It's a tie"
-    else if (playerSelection == "rock" && computerSelection == "paper")
+    else if (playerSelection == "r" && computerSelection == "p")
+    {
+        score[1]++
         return "You Lose! Paper beats Rock"
-    else if (playerSelection == "rock" && computerSelection == "scissors")
-        return "You win! Rock beats Scissors"
-    else if (playerSelection == "paper" && computerSelection == "rock")
-        return "You win! Paper beats Rock"
-    else if (playerSelection == "paper" && computerSelection == "scissors")
-        return "You Lose! Scissors beats Paper"
-    else if (playerSelection == "scissors" && computerSelection == "rock")
-        return "You Lose! Rock beats Scissors"
-    else if (playerSelection == "scissors" && computerSelection == "paper")
-        return "You win! Scissors beats Paper"
-}
-
-
-function calcScore(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) 
-        return 0
-    else if ((playerSelection == "rock" && computerSelection == "paper") || 
-                (playerSelection == "paper" && computerSelection == "scissors") || 
-                (playerSelection == "scissors" && computerSelection == "rock"))
-        return -1
-    else if ((playerSelection == "rock" && computerSelection == "scissors") || 
-                (playerSelection == "paper" && computerSelection == "rock") || 
-                (playerSelection == "scissors" && computerSelection == "paper"))
-        return 1
-}
-
-   
-
-
-function game(){
-    let score = 0;
-    for(let i = 0; i < 5; i++){
-        const playerSelection = getPlayerChoice()
-        const computerSelection = getComputerChoice() 
-
-        score += calcScore(playerSelection, computerSelection)
-
-        console.log("You chose " + playerSelection + " and coputer chose " + computerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-        console.log("\n")   
     }
-    if(score == 0)
-        console.log("************* It's a tie *************")
-    else if(score >= 1)
-        console.log("************* You win! *************")
-    else if(score <= -1)
-        console.log("************* You lose! *************")
+    else if (playerSelection == "r" && computerSelection == "s")
+    {
+        score[0]++
+        return "You win! Rock beats Scissors"
+    }
+    else if (playerSelection == "p" && computerSelection == "r")
+    {
+        score[0]++
+        return "You win! Paper beats Rock"
+    }
+    else if (playerSelection == "p" && computerSelection == "s")
+    {
+        score[1]++
+        return "You Lose! Scissors beats Paper"
+    }
+    else if (playerSelection == "s" && computerSelection == "r")
+    {
+        score[1]++
+        return "You Lose! Rock beats Scissors"
+    }
+    else if (playerSelection == "s" && computerSelection == "p")
+    {
+        score[0]++
+        return "You win! Scissors beats Paper"
+    }
 }
 
-game()
+function play(playerSelection){
+    if(score[0] < 3 && score[1] < 3) {
+        let computerSelection = getComputerChoice()
+        //console.log(playRound(playerSelection, computerSelection))
+        gameCommentDiv.innerHTML = '<p>' + playRound(playerSelection, computerSelection) +'</p>'
+        scoreDiv.innerHTML = '<p>' + score[0] +" | "+ score[1] +'</p>'
+        if (score[0] == 3 || score[1] == 3)
+            endGame()
+    }
+    else
+        endGame()
+}
+
+function endGame(){
+    btnR.disabled = true
+    btnP.disabled = true  
+    btnS.disabled = true
+    retryDiv.innerHTML = '<button id="rt" class="btn">Retry</button>'
+    document.getElementById("rt").addEventListener('click', () => newGame())       
+    if(score[0] > score[1])
+        gameCommentDiv.innerHTML = "you won the game gg !!!"
+    else 
+        gameCommentDiv.innerHTML = "you lost the game sad :("
+}
+
+
+btnR.addEventListener('click', () => play("r"))
+btnP.addEventListener('click', () => play("p"))
+btnS.addEventListener('click', () => play("s"))
+
+
+
